08042cafff8ef9a9bf4d1cf88b55b196407b5e68
Yuhan Ma <admgmtt@gmail.com>
2024-08-02 17:45:45 +0800

    [Initial commit]

---
+++ NibbleOnAstarion/Mods/NibbleOnAstarion/ScriptExtender/Lua/BootstrapServer.lua
@@ -0,0 +1,21 @@
+Ext.Require("Main.lua")
+
+print("My First SE mod loaded succesfully")
+
+local function CheckNibbleTriggers()
+
+
+    --math.randomseed(os.clock()*100000000000)
+    --if randbool = rnd() < 0.5 then NibbleOnAstarion()
+
+    Ext.Entity.Get("0133f2ad-e121-4590-b5f0-a79413919805")
+    
+end
+
+local function NibbleOnAstarion()
+
+    local astarion = Ext.Entity.Get("0133f2ad-e121-4590-b5f0-a79413919805")
+    local player = Ext.Entity.Get("0133f2ad-e121-4590-b5f0-a79413919805")
+end
+
+--Ext.Osiris.RegisterListener("PROC_CampNight_ClearCampNight", 1, "before", CheckNibbleTriggers)
---
+++ NibbleOnAstarion/Mods/NibbleOnAstarion/ScriptExtender/Lua/Main.lua
@@ -0,0 +1,24 @@
+local astarion = "S_Player_Astarion_c7c13742-bacd-460a-8f65-f864fe41f255"
+
+local function CheckNibbleTriggers()
+
+    -- Tag: 469b943c-d986-470f-bd2e-84077b458fdc
+    -- "Set on the character Astarion will be feeding on tonight."   << This is night nibble trigger
+ 
+    -- Tag: d7819489-bbe1-4d7c-96f2-d01c7c336115
+    -- "Astarion turns the player's character into a spawn"
+
+    
+    if
+
+    -- math.randomseed(os.clock()*100000000000)
+    -- if randbool = rnd() < 0.5 then NibbleOnAstarion()
+
+    Ext.Entity.Get("0133f2ad-e121-4590-b5f0-a79413919805")
+end
+
+local function NibbleOnAstarion()
+
+    local astarion = Ext.Entity.Get("0133f2ad-e121-4590-b5f0-a79413919805")
+    local player = Ext.Entity.Get("0133f2ad-e121-4590-b5f0-a79413919805")
+end
