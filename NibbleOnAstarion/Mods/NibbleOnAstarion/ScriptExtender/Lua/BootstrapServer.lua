49c0f0e927ba24f0158ead96482005715376c26a
Yuhan Ma <admgmtt@gmail.com>
2024-08-05 17:55:50 +0800

    [Update print info]

--- NibbleOnAstarion/Mods/NibbleOnAstarion/ScriptExtender/Lua/BootstrapServer.lua
+++ NibbleOnAstarion/Mods/NibbleOnAstarion/ScriptExtender/Lua/BootstrapServer.lua
@@ -6,31 +6,31 @@ Ext.Utils.Print("NibbleOnAstarian mod loaded succesfully")
 local function OnSessionLoaded()
 
     Ext.Utils.Print("NibbleOnAstarian On Session Loaded")
-    Ext.Osiris.RegisterListener("LongRestStarted", 0, "before", PrintRestStartBefore)
-    Ext.Osiris.RegisterListener("LongRestStarted", 0, "after", PrintRestStartAfter)
-    Ext.Osiris.RegisterListener("LongRestFinished", 0, "before", PrintRestFinishBefore)
-    Ext.Osiris.RegisterListener("LongRestFinished", 0, "after", PrintRestFinishAfter)
+    Ext.Osiris.RegisterListener("LongRestStarted", 0, "before", PrintRestStartBefore())
+    Ext.Osiris.RegisterListener("LongRestStarted", 0, "after", PrintRestStartAfter())
+    Ext.Osiris.RegisterListener("LongRestFinished", 0, "before", PrintRestFinishBefore())
+    Ext.Osiris.RegisterListener("LongRestFinished", 0, "after", PrintRestFinishAfter())
 
 end
 
 local function PrintRestStartBefore()
     Ext.Utils.Print("NibbleOnAstarian -- Rest Start Before")
-    CheckNibbleTriggers
+    CheckNibbleTriggers(Osi.GetHostCharacter())
 end
                       
 local function PrintRestStartBefore()       
     Ext.Utils.Print("NibbleOnAstarian -- Rest Start After")
-    CheckNibbleTriggers
+    CheckNibbleTriggers(Osi.GetHostCharacter())
 end
 
 local function PrintRestFinishBefore()
     Ext.Utils.Print("NibbleOnAstarian -- Rest Finish Before")
-    CheckNibbleTriggers
+    CheckNibbleTriggers(Osi.GetHostCharacter())
 end
 
 local function PrintRestFinishAfter()    
     Ext.Utils.Print("NibbleOnAstarian -- Rest Finish After")
-    CheckNibbleTriggers
+    CheckNibbleTriggers(Osi.GetHostCharacter())
 end
 
 
--- NibbleOnAstarion/Mods/NibbleOnAstarion/ScriptExtender/Lua/Main.lua
+++ NibbleOnAstarion/Mods/NibbleOnAstarion/ScriptExtender/Lua/Main.lua
@@ -1,27 +1,32 @@
-local astarion = "S_Player_Astarion_c7c13742-bacd-460a-8f65-f864fe41f255"
+    -- Tag: "S_Player_Astarion_c7c13742-bacd-460a-8f65-f864fe41f255"
 
     -- Tag: 469b943c-d986-470f-bd2e-84077b458fdc
-    -- "Set on the character Astarion will be feeding on tonight."   << the night nibble trigger
+    -- "Set on the character Astarion will be feeding on tonight." << the night nibble trigger
  
     -- Tag: d7819489-bbe1-4d7c-96f2-d01c7c336115
-    -- "Astarion turns the player's character into a spawn" <<
+    -- "Astarion turns the player's character into a spawn"
 
     -- Tag: 97a839f0-9923-455a-b21f-1c1314fda711
     -- "Player has followed up on Astarion as their partner after Black Mass" << a tag that check if player have talked about being turned
 
-
-local function CheckNibbleTriggers()
-    
-    Ext.Utils.Print("NibbleOnAstarian Nibble Flag:"+ Osi.GetFlag("469b943c-d986-470f-bd2e-84077b458fdc", GetHostCharacter()))
-   
+---@param PlayerCharacter string
+local function NibbleOnAstarion(PlayerCharacter)
     -- math.randomseed(os.clock()*100000000000)
     -- if randbool = rnd() < 0.5 then NibbleOnAstarion()
     
+    Osi.ApplyStatus(PlayerCharacter, "ASTARION_HAPPY", -1)
+    Osi.ApplyStatus("c7c13742-bacd-460a-8f65-f864fe41f255", "ASTARION_WEAK", -1)
 end
 
 ---@param PlayerCharacter string
-local function NibbleOnAstarion(PlayerCharacter)
+local function CheckNibbleTriggers(PlayerCharacter)
+    local flagNibble = Osi.GetFlag("469b943c-d986-470f-bd2e-84077b458fdc", PlayerCharacter)
+    local flagIsSpawn = Osi.GetFlag("d7819489-bbe1-4d7c-96f2-d01c7c336115", PlayerCharacter)
+    local flagTalked = Osi.GetFlag("97a839f0-9923-455a-b21f-1c1314fda711", PlayerCharacter)
 
-    Osi.ApplyStatus(PlayerCharacter, "ASTARION_HAPPY", -1)
-    Osi.ApplyStatus("c7c13742-bacd-460a-8f65-f864fe41f255", "ASTARION_WEAK", -1)
+    -- Osi.IteratePlayerCharacters
+    Ext.Utils.Print("Check Character: " .. PlayerCharacter)
+    Ext.Utils.Print("Check Flag - Nibble: " .. flagNibble)
+    Ext.Utils.Print("Check Flag - IsSpawn: " .. flagIsSpawn)
+    Ext.Utils.Print("Check Flag - Talked: " .. flagTalked)  
 end
