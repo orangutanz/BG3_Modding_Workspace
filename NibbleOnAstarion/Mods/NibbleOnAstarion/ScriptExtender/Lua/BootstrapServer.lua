5d007e6c4cb6ee450ffe29d9f86f4f32014967d6
Yuhan Ma <admgmtt@gmail.com>
2024-08-06 11:16:46 +0800

    [Ready]

--- NibbleOnAstarion/Mods/NibbleOnAstarion/ScriptExtender/Lua/BootstrapServer.lua
+++ NibbleOnAstarion/Mods/NibbleOnAstarion/ScriptExtender/Lua/BootstrapServer.lua
@@ -1,15 +1,8 @@
-Ext.Require("Main.lua")
-print("NibbleOnAstarian mod loaded succesfully")
-Ext.Utils.Print("..............NibbleOnAstarian mod loaded succesfully")
+Ext.Require("Server/Main.lua")
 
-local function OnSessionLoaded()
-    Print("................NibbleOnAstarian Session Loaded ....................")
-    Ext.Utils.Print("...............NibbleOnAstarian On Session Loaded..........")
-    --Ext.Osiris.RegisterListener("LongRestStarted", 0, "before", PrintRestStartBefore())
-    --Ext.Osiris.RegisterListener("LongRestStarted", 0, "after", PrintRestStartAfter())
-    --Ext.Osiris.RegisterListener("LongRestFinished", 0, "before", PrintRestFinishBefore())
-    --Ext.Osiris.RegisterListener("LongRestFinished", 0, "after", PrintRestFinishAfter())
-
+function OnSessionLoaded()
+    --_P("NibbleOnAstarion Mod Loaded")
+    Ext.Osiris.RegisterListener("UserCharacterLongRested", 2, "after", function(character, isFullRest) CheckFlags(character) end)
 end
 
 Ext.Events.SessionLoaded:Subscribe(OnSessionLoaded)
--- NibbleOnAstarion/Mods/NibbleOnAstarion/ScriptExtender/Lua/Server/Main.lua
+++ NibbleOnAstarion/Mods/NibbleOnAstarion/ScriptExtender/Lua/Server/Main.lua
@@ -1,46 +1,26 @@
-    -- Tag: "S_Player_Astarion_c7c13742-bacd-460a-8f65-f864fe41f255"
-
-    -- Tag: 469b943c-d986-470f-bd2e-84077b458fdc
-    -- "Set on the character Astarion will be feeding on tonight." << the night nibble trigger
- 
-    -- Tag: d7819489-bbe1-4d7c-96f2-d01c7c336115
-    -- "Astarion turns the player's character into a spawn"
-
-    -- Tag: 97a839f0-9923-455a-b21f-1c1314fda711
-    -- "Player has followed up on Astarion as their partner after Black Mass" << a tag that check if player have talked about being turned
-
-local function NibbleOnAstarion()
-    -- math.randomseed(os.clock()*100000000000)
-    -- if randbool = rnd() < 0.5 then NibbleOnAstarion()
-    
-    Osi.ApplyStatus(Osi.GetHostCharacter(), "ASTARION_HAPPY", -1)
+function NibbleOnAstarion(char)
+    _P("Nibble on Astarion!")
+    Osi.ApplyStatus(char, "ASTARION_HAPPY", -1)
     Osi.ApplyStatus("c7c13742-bacd-460a-8f65-f864fe41f255", "ASTARION_WEAK", -1)
 end
 
-local function CheckNibbleTriggers()
-    local flagNibble = Osi.GetFlag("469b943c-d986-470f-bd2e-84077b458fdc", Osi.GetHostCharacter()) --int
-    local flagIsSpawn = Osi.GetFlag("d7819489-bbe1-4d7c-96f2-d01c7c336115", Osi.GetHostCharacter()) --int
-    local flagTalked = Osi.GetFlag("97a839f0-9923-455a-b21f-1c1314fda711", Osi.GetHostCharacter()) --int
 
-    -- Osi.IteratePlayerCharacters
-    Ext.Utils.Print("Check Character: " .. Osi.GetHostCharacter())
-    Ext.Utils.Print("Check Flag - Nibble: " .. flagNibble)
-    Ext.Utils.Print("Check Flag - IsSpawn: " .. flagIsSpawn)
-    Ext.Utils.Print("Check Flag - Talked: " .. flagTalked)  
-end
+function CheckFlags(Char)
+    
+    local flagIsSpawn = Osi.GetFlag("d7819489-bbe1-4d7c-96f2-d01c7c336115", Char) --int
 
-local function PrintRestStartBefore()
-    Ext.Utils.Print("NibbleOnAstarian -- Rest Start Before")
-end
-                      
-local function PrintRestStartBefore()       
-    Ext.Utils.Print("NibbleOnAstarian -- Rest Start After")
-end
+    if flagIsSpawn == 0 then
+        return
+    end
 
-local function PrintRestFinishBefore()
-    Ext.Utils.Print("NibbleOnAstarian -- Rest Finish Before")
-end
+    local flagNibble = Osi.GetFlag("469b943c-d986-470f-bd2e-84077b458fdc", Char) --int
+    local flagTalked = Osi.GetFlag("97a839f0-9923-455a-b21f-1c1314fda711", Char) --int
+    local flagBreakUp = Osi.GetFlag("5a60943f-979b-4120-9b60-9e9b29529402", Char) --int
 
-local function PrintRestFinishAfter()    
-    Ext.Utils.Print("NibbleOnAstarian -- Rest Finish After")
+    Ext.Utils.Print("Spawn: " .. Char)
+    Ext.Utils.Print("Flags - Nibble:" .. flagNibble .. " Talked:" .. flagTalked .. " BreakUp:" .. flagBreakUp)
+
+    if flagNibble == 1 and flagTalked == 1 and flagBreakUp == 0 then
+        NibbleOnAstarion(Char)
+    end
 end
